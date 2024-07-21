68fa842d55887eabbed9e6e81bd26c8a0b8071a4
Dulan Malith <dulan@skoolbag.com.au>
2024-07-21 19:53:21 +0530

    Fixing deploying issues

--- src/index.js
+++ src/index.js
@@ -2,21 +2,24 @@ require('dotenv').config();
 const express= require('express');
 const bodyParser =  require('body-parser');
 const authRouter = require('./auth/auth');
-const weatherRouter = require('./weather/weather');
-const authMiddlware = require('./middleware/authMiddleware');
 const cors = require('cors');
+const http = require('http');
 
 const {setupWebSocketServer} = require('./websocket/websocket')
 const app = express();
+// Create HTTP server
+const server = http.createServer(app);
+
+// Setup WebSocket server
+setupWebSocketServer(server);
 const port = process.env.port || 3000;
 
 
+setupWebSocketServer(server);
 app.use(bodyParser.json());
 app.use(cors())
 
 app.use('/api/auth', authRouter);
-const server = app.listen(port, () => {
+server.listen(port, () => {
     console.log('server running...');
 });
-
-setupWebSocketServer(server);
